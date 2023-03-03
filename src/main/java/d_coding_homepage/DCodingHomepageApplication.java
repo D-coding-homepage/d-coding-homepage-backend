552f82d62443d55f4dc8007041f3f135e7b14772
최재민 <panzzang518@naver.com>
2023-03-03 12:23:52 +0900

    initial commit

---
+++ src/main/java/d_coding_homepage/DCodingHomepageApplication.java
@@ -0,0 +1,13 @@
+package d_coding_homepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DCodingHomepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DCodingHomepageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/d_coding_homepage/DCodingHomepageApplicationTests.java
@@ -0,0 +1,13 @@
+package d_coding_homepage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DCodingHomepageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
